ffdb0c27a11370de55787ac4700634b13f7dd61d
FakharElyes <elyes.fakhar@esprit.tn>
2022-04-11 14:51:55 +0100

    prepareing fonts

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  { Text, View, SafeArea, FlatList } from 'react-native'
+import {useState} from 'react'
+
+function Home() {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home
