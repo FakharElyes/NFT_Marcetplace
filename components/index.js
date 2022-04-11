79367bac882507b8f0a00b4c5e36829de700a8b4
FakharElyes <elyes.fakhar@esprit.tn>
2022-04-11 16:30:48 +0100

    info

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import FocussedStatusBar from "./FocussedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+
+export {
+    HomeHeader,NFTCard,FocussedStatusBar,CircleButton, RectButton
+}
