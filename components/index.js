f20a93a3b04d2241a6f50f0da3f505ad62d85abf
FakharElyes <elyes.fakhar@esprit.tn>
2022-04-12 14:54:55 +0100

    details screen

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import HomeHeader from "./HomeHeader";
 import FocussedStatusBar from "./FocussedStatusBar";
 import NFTCard from "./NFTCard";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 import { CircleButton, RectButton } from "./Button";
-
+import { SubInfo } from "./SubInfo";
 
 export {
-    HomeHeader,NFTCard,FocussedStatusBar,CircleButton, RectButton
+    HomeHeader, NFTCard, FocussedStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo
 }
